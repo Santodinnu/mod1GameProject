d98a77a75b2475333618926991c3f02e045f0aa1
santodinnu <shantoshi@gmail.com>
2023-11-09 11:46:27 -0500

    added title to html, linked stylesheet and script file. also wrote initial pseudocode to script file

---
+++ script.js
@@ -0,0 +1,4 @@
+// initially create flex to have the memory game
+// have 4 levels of the game
+// include time duration to choose whether game is won or lost
+// make it interactive by inputing name from the player
