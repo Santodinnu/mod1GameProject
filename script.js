44842fd03688eee0548504fb13047fdad7412011
santodinnu <shantoshi@gmail.com>
2023-11-09 14:42:57 -0500

    Added 16boxes for the game and made some styling changes

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-// initially create flex to have the memory game
+// initially create grid to have the memory game
 // have 4 levels of the game
 // include time duration to choose whether game is won or lost
 // make it interactive by inputing name from the player
+
+
+const mainGridEl = document.querySelector(".mainGrid");
